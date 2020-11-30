5a34a6720ee97ceed2b76c4f1661454a1598b49d
mike haberman <haberman@illinois.edu>
2020-11-30 14:52:48 -0800

    init

---
+++ lessons/ReadMe.txt
@@ -0,0 +1,9 @@
+
+
+
+Tag
+Course:Classroom:Lesson
+
+keep lowercase
+dmap:projects:baseball
+p4ds:bootcamp:functions
---
+++ resources/pre_commit.sh
@@ -0,0 +1,28 @@
+#!/usr/local/bin/zsh
+#!/bin/bash
+# get all the files that are ready to commit
+# user has executed git add . 
+
+root="/Users/mikeh/home/projects/CodeStoryLessons"
+out=$(git diff-index --cached Head | cut -f2 | grep lessons | cut -f1-4 -d/ | uniq)
+for i in $out; do
+  # lessons/dmap/projects/project-m3
+  SRC_DIR=$root/$i
+  dir=$(echo ${i}| cut -f1 -d/)
+  if [ ${dir} = 'lessons' ]; then
+    LESSON=$(echo ${i}| cut -f4 -d/)
+    echo "doing ${i} ${dir} ${LESSON}"
+    #echo "${SRC_DIR}"
+
+    gtar --sort=name --owner=root:0 --group=root:0 --mtime='UTC 2021-01-01' -cvf ${LESSON}.tar --exclude='*.ipynb' --exclude='*.gz' ${SRC_DIR} &> /tmp/log.txt
+    gzip -n ${LESSON}.tar
+    /bin/mv ${LESSON}.tar.gz ${SRC_DIR}
+    echo 'created'
+    ls -la ${SRC_DIR}/*.gz
+    git add ${SRC_DIR}
+    echo 'be sure to git commit'
+
+  else
+    echo "skip ${i}"
+  fi
+done
