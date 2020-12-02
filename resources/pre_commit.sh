fd7ae05228c0e3e4dee4b54bbda3642cec9a2471
mike haberman <haberman@illinois.edu>
2020-12-02 15:57:14 -0800

    intro

--- resources/pre_commit.sh
+++ resources/pre_commit.sh
@@ -8,31 +8,37 @@ root="/Users/mikeh/home/projects/CodeStoryLessons"
 if [ $# -eq 0 ]; then
   out=$(git diff-index --cached Head | cut -f2 | grep lessons | cut -f1-4 -d/ | uniq)
 else
+  echo 'process params'
   out="$1"
   # convert tag dmap:projects:project-m3 into lessons/dmap/projects/project-m3
 fi
 
-echo "doing ${out}"
-for i in $out; do
-  # lessons/dmap/projects/project-m3
-  SRC_DIR=$root/$i
-  dir=$(echo ${i}| cut -f1 -d/)
+IFS=' '
+while read -r line;do
+  echo "Processing ${line}"
+  SRC_DIR=$root/$line
+  dir=$(echo ${line}| cut -f1 -d/)
+  echo "Testing ${dir}"
+
   if [ ${dir} = 'lessons' ]; then
-    LESSON=$(echo ${i}| cut -f4 -d/)
-    echo "doing ${TODO} ${i} ${dir} ${LESSON}"
+    LESSON=$(echo ${line}| cut -f4 -d/)
+    echo "doing ${TODO} ${line} ${dir} ${LESSON}"
     #echo "${SRC_DIR}"
     # cd to the directory so tar only keeps relative path
     cd "${root}"
-    gtar --sort=name --owner=root:0 --group=root:0 --mtime='UTC 2021-01-01' -cvf ${LESSON}.tar --exclude='*.ipynb' --exclude='*.gz' ${i} &> /tmp/log.txt
+    gtar --sort=name --owner=root:0 --group=root:0 --mtime='UTC 2021-01-01' \
+         -cvf ${LESSON}.tar \
+         --exclude='*.ipynb' --exclude='*.gz' ${line} &> /tmp/log.txt
     gzip -n ${LESSON}.tar
     /bin/mv ${LESSON}.tar.gz ${SRC_DIR}
-    echo 'created'
     ls -la ${SRC_DIR}/*.gz
     git add ${SRC_DIR}
     echo 'tar file added via git'
     echo 'be sure to git commit'
 
   else
-    echo "skip ${i}"
+    echo "skip ${line}"
   fi
-done
+
+done <<< "$out"
+
