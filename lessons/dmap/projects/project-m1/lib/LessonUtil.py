c1e84ede24b222394c3dbc4ceb8057f14411c5b7
mike haberman <haberman@illinois.edu>
2021-03-27 07:36:18 -0700

    w11

---
+++ lessons/dmap/projects/project-m1/lib/LessonUtil.py
@@ -0,0 +1,33 @@
+#
+# common code given to the students
+# only edit the docker source, this gets copied into distribution
+#
+
+import os
+
+
+def ensure_path():
+    fq = os.path.dirname(os.path.abspath(__file__))
+    d_dir = "{:s}/../data".format(fq)
+    if os.path.isdir(d_dir):
+        return d_dir
+    d_dir = "{:s}/data".format(fq)
+    if os.path.isdir(d_dir):
+        return d_dir
+
+    print('unable to find data directory')
+    return ''
+
+DATA_DIR = ensure_path()
+
+
+def path_for_data(filename):
+    base = os.path.basename(filename)
+    return "{:s}/{:s}".format(DATA_DIR, base)
+
+
+def read_data_file(filename):
+    fn = path_for_data(filename)
+    with open(fn, 'r') as fd:
+        return fd.read()
+
